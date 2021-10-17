1462e61059c813301776fa8a5f6441cdafafb0d1
Luisa Go <luisaabigailgo@gmail.com>
2021-10-17 20:11:42 +0800

    initial commit

---
+++ mapquest_parse-json_3.py
@@ -0,0 +1,16 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "ci3HmNJSBN1I6UYPtHDrp3Nhog2fHoVF"
+
+
+while True:
+ orig = input("Starting Location: ")
+ dest = input("Destination: ")
+ url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+ print("URL: " + (url))
+ json_data = requests.get(url).json()
+ json_status = json_data["info"]["statuscode"]
+ if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
